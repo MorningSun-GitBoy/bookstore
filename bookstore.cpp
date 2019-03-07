84638367343d75fe688e7b7cd7b71e99d8ad6bea
HuaWei-01 <AndyWolfcf2013@163.com>
2019-03-07 19:24:00 +0800

    because of changing ,there is some touble 由于代码改动，出现了一些问题

---
+++ bookstore.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include "Appuser.h"
+#include "book.h"
+int main(){
+      int i=0,buyerid,flag;
+      book * cbook[2];//指针数组存放book对象地址
+      address * addr[3];
+      layfolk b1("张某",1,addr,0)
+      return 0;
+}
